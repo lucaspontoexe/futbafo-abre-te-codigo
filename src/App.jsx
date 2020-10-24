7f332b80fbaba26a8a82ec9b3ff59ee3e66000e3
lucaspontoexe <ltx16@hotmail.com>
2020-10-24 12:58:02 -0300

    clicar no botão escolhe cartas aleatórias

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 import dataset from "./dataset.json";
 const sampleData = dataset.slice(0, 5);
 
+/**
+ * Pick some cards from the deck
+ * @param {array} deck
+ */
+function pickCards(deck, quantity = 3) {
+  let temp = [];
+  for (let index = 0; index < quantity; index++) {
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    temp.push(deck[randomIndex]);
+  }
+  return temp;
+}
+
 function App() {
+  const [cards, setCards] = useState([]);
+
   return (
     <div className="App">
-      <h1>habemus app</h1>
+      <h1>react bafo</h1>
+      <p>calma que hoje à noite a coisa se torna um pouco mais visual</p>
 
       <p>algum jeito de representar cartas [todo: import csv]</p>
 
-      <button>
+      <button onClick={() => setCards(pickCards(dataset))}>
         se clicar aqui, algumas [tipo, três] vão pra essa lista aqui
       </button>
 
-      <h2>samples:</h2>
-
-      <ul>
-        {sampleData.map((item) => (
-          <li>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
+      <h2>sample:</h2>
+      <pre>{JSON.stringify(dataset[0], null, 4)}</pre>
 
       <h2>cartas escolhidas:</h2>
       <ul>
-        <li>nome da carta</li>
-        <li>nome da outra carta</li>
-        <li>terceira carta</li>
+        {cards.map((card) => (
+          <li>{card.nome}</li>
+        ))}
       </ul>
     </div>
   );
