dbdd1f0a892b2a3de4783290c2b5e3cd92482b82
lucaspontoexe <ltx16@hotmail.com>
2020-10-24 14:48:41 -0300

    format, add placeholder card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
 import { useEffect, useState } from "react";
 import dataset from "./dataset.json";
 import "./App.css";
-import hand from './images/hand.png';
+import hand from "./images/hand.png";
+import rectangle from "./images/rectangle9.svg";
 import { pickCards } from "./pickCards";
 
 function App() {
-  const [cards, setCards] = useState(JSON.parse(localStorage.getItem('cards')) || []);
+  const [cards, setCards] = useState(
+    JSON.parse(localStorage.getItem("cards")) || []
+  );
 
   useEffect(() => {
-    localStorage.setItem('cards', JSON.stringify(cards))
-  }, [cards])
+    localStorage.setItem("cards", JSON.stringify(cards));
+  }, [cards]);
 
   return (
     <div className="App">
-      
       <div className="wrapper">
+        <div className="cards-holder">
+          <img src={rectangle} alt="Carta" />
+          <img src={rectangle} alt="Carta" />
+        </div>
 
-        <img src={hand} alt="Mão"/>
+        <img src={hand} alt="Mão" />
       </div>
 
       <button onClick={() => setCards([...cards, ...pickCards(dataset)])}>
         se clicar aqui, algumas [tipo, três] vão pra essa lista aqui
       </button>
 
-
       <h2>cartas escolhidas:</h2>
       <ul>
         {cards.map((card) => (
-          <li key={card.nome}>{card.nome}: {card.titulo}</li>
+          <li key={card.nome}>
+            {card.nome}: {card.titulo}
+          </li>
         ))}
       </ul>
-
-
     </div>
   );
 }
