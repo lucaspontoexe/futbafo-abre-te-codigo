53d118a21a60cdc92c18eb00058f0bfc77c7af37
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 14:57:39 -0300

    estilização página você sabia

--- src/pages/VoceSabia.jsx
+++ src/pages/VoceSabia.jsx
@@ -1,8 +1,11 @@
+import logo from 'images/LOGO_com_Donas_da_Bola_Roxo.png';
+import './VoceSabia.scss';
+
 export default function VocêSabia(params) {
   // tá com cara de componente isso
   return (
-    <section>
-      [logo]
+    <section id="voce-sabia">
+      <img src={logo} alt="Logo Futebafo Donas da Bola"/>
       <h1>Você Sabia?</h1>
       <p>
         Por 40 anos (!!!), entre 1940 e 1980, as mulheres foram proibidas de
@@ -12,7 +15,7 @@ export default function VocêSabia(params) {
         #futebolfeminino #donasdabola #futebafo #museudofutebol
       </div>
       <div className="logos"></div>
-      <p>
+      <p className="bottom-text">
         Essa foi só uma das dificuldades que nossas guerreiras enfrentaram para
         que o futebol feminino brasileiro chegasse aos títulos. Por isso, essa
         edição do jogo é dedicado a elas! Complete seu álbum com as figurinhas
