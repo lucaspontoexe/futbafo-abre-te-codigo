291fc157b829d1fcd3e849455c2aa7fe94bfdf11
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 13:45:31 -0300

    os links funcionam

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,37 @@
-import "./NavBar.scss";
+import { Link } from "react-router-dom";
 import profile from "images/icons/icon_perfil.png";
 import howtoplay from "images/icons/icon_comojogar.png";
 import game from "images/icons/icon_bafo.png";
 import album from "images/icons/icon_album.png";
+import "./NavBar.scss";
 
 export default function Header(params) {
   return (
     <nav className="main-nav">
       <ul>
         <li className="active">
-          <img src={profile} alt="Pefil" />
-          <span>Perfil</span>
+          <Link to="/profile">
+            <img src={profile} alt="Pefil" />
+            <span>Perfil</span>
+          </Link>
         </li>
         <li>
-          <img src={howtoplay} alt="Como Jogar" />
-          <span>Como Jogar?</span>
+          <Link to="/tutorial">
+            <img src={howtoplay} alt="Como Jogar" />
+            <span>Como Jogar?</span>
+          </Link>
         </li>
         <li>
-          <img src={game} alt="cartas" />
-          <span>Bafo!</span>
+          <Link to="/game">
+            <img src={game} alt="cartas" />
+            <span>Bafo!</span>
+          </Link>
         </li>
         <li>
-          <img src={album} alt="álbum" />
-          <span>Álbum</span>
+          <Link to="/album">
+            <img src={album} alt="álbum" />
+            <span>Álbum</span>
+          </Link>
         </li>
       </ul>
     </nav>
