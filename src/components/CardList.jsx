fbb469937f6157aa0ae8246909d9e63a4056f075
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 03:34:36 -0300

    wip cardlist styling

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,16 +1,34 @@
 // import dataset from "../dataset.json";
 import sampleCard from "images/rectangle9.svg";
+import check from "images/icons/sinal_check.png";
+
+import "./CardList.scss";
 
 export default function CardList(params) {
   return (
-    <section>
+    <section className="card-list">
       <div className="card-wrapper">
         <img src={sampleCard} alt="Carta" />
       </div>
 
       <div className="card-wrapper selected">
         <img src={sampleCard} alt="Carta" />
+        <div className="check">
+          <img src={check} alt=""/>
+        </div>
       </div>
+
+      <div className="card-wrapper">
+        <img src={sampleCard} alt="Carta" />
+      </div>
+
+
+      <div className="card-wrapper">
+        <img src={sampleCard} alt="Carta" />
+      </div>
+
+
+    
     </section>
   );
 }
