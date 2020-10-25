62252dcd19bd1d26073ebf56d02579707e1b1549
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 01:01:27 -0300

    anotações sobre o jogo

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,31 @@
 import Header from "components/Header";
 
 export default function Game(params) {
-    return (<><Header/></>);
+    return (<>
+    {/* TODO: navbar em vez de header? */}
+    <Header/>
+
+    <h1>Comece a jogar!</h1>
+    <p>Bata no monte e tente virar as figurinhas</p>
+
+    <div className="game">
+        {/* 
+
+        deck will have some [how many user has chosen] random cards from cpu [dataset] +
+        user-chosen ones
+        
+
+        cards will have fixed width in pixels
+        position them at the middle of screen using JS to get the offset [screen middle - card 1/2 width and height],
+        varying a little bit using random()
+
+        on tap, navigator.vibrate()
+        
+        flip some cards [maybe css 3d transform?]
+
+        make position and rotation go random using transform
+
+        */}
+    </div>
+    </>);
 }
