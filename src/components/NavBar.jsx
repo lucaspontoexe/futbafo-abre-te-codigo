5a370abbf7c880fdca918b87bcf0bd47405a6847
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 03:07:04 -0300

    add icons, stylize navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
+import "./NavBar.scss";
+import profile from "images/icons/icon_perfil.png";
+import howtoplay from "images/icons/icon_comojogar.png";
+import game from "images/icons/icon_bafo.png";
+import album from "images/icons/icon_album.png";
+
 export default function Header(params) {
   return (
-    <nav>
+    <nav className="main-nav">
       <ul>
-        <li>Perfil</li>
-        <li>Como Jogar?</li>
-        <li>Bafo!</li>
-        <li>Álbum</li>
+        <li className="active">
+          <img src={profile} alt="Pefil" />
+          <span>Perfil</span>
+        </li>
+        <li>
+          <img src={howtoplay} alt="Como Jogar" />
+          <span>Como Jogar?</span>
+        </li>
+        <li>
+          <img src={game} alt="cartas" />
+          <span>Bafo!</span>
+        </li>
+        <li>
+          <img src={album} alt="álbum" />
+          <span>Álbum</span>
+        </li>
       </ul>
     </nav>
   );
