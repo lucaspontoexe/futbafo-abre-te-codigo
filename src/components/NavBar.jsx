64b56e9f9ccbd132803e3b7c37f7b699db5a722b
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 13:52:21 -0300

    navbar reacts to route changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import profile from "images/icons/icon_perfil.png";
 import howtoplay from "images/icons/icon_comojogar.png";
 import game from "images/icons/icon_bafo.png";
 import album from "images/icons/icon_album.png";
 import "./NavBar.scss";
 
-export default function Header(params) {
+const links = [
+  { location: "/profile", name: "Perfil", image: profile },
+  { location: "/tutorial", name: "Como Jogar?", image: howtoplay },
+  { location: "/game", name: "Bafo!", image: game, alt: "Cartas" },
+  {
+    location: "/album",
+    name: "Álbum",
+    image: album,
+    alt: "Álbum de Figurinhas",
+  },
+];
+
+export default function Header() {
+  const { pathname } = useLocation();
+
   return (
     <nav className="main-nav">
       <ul>
-        <li className="active">
-          <Link to="/profile">
-            <img src={profile} alt="Pefil" />
-            <span>Perfil</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/tutorial">
-            <img src={howtoplay} alt="Como Jogar" />
-            <span>Como Jogar?</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/game">
-            <img src={game} alt="cartas" />
-            <span>Bafo!</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/album">
-            <img src={album} alt="álbum" />
-            <span>Álbum</span>
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.location} className={pathname === link.location ? "active" : ""}>
+            <Link to={link.location}>
+              <img src={link.image} alt={link.alt || link.name} />
+              <span>{link.name}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
