161b2a466642802c8772850e7ea2870a4e62915f
lucaspontoexe <ltx16@hotmail.com>
2020-10-25 21:56:33 -0300

    add texture

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,6 @@
 import NavBar from "components/NavBar";
+import { useState } from "react";
+import bluecard from 'images/cards/blue.png';
 
 function CardsDisplay(params) {
   // usado pra mostrar as cartas que ganhou
@@ -18,9 +20,12 @@ const cardWidth = 60;
 const maxDistance = 90;
 
 export default function Game(params) {
+  //ia ser interessante mudar esse nome hein
+  const [flipcard, setflip] =  useState(false);
+  const [revealed, setRevealed] = useState(false);
+  
   return (
     <>
-      {/* TODO: navbar em vez de header? */}
       <NavBar />
 
       <h1>Comece a jogar!</h1>
@@ -45,30 +50,38 @@ export default function Game(params) {
 
         */}
 
-        <div className="card" style={processCardStyle(false)}></div>
-        <div className="card" style={processCardStyle(false)}></div>
-        <div className="card" style={processCardStyle(false)}></div>
-        <div className="card" style={processCardStyle(false)}></div>
+        <div className="card" style={processCardStyle(flipcard)} style={{}}></div>
+        <div className="card" style={processCardStyle(flipcard)}></div>
+        <div className="card" style={processCardStyle(flipcard)}></div>
+        <div className="card" style={processCardStyle(flipcard)}></div>
       </div>
+
+      <button onClick={() => setflip(true)}>ala</button>
     </>
   );
 }
 
+/**
+ * get card style for flipping
+ * @param {boolean} flipped 
+ * @returns {import("react").CSSProperties}
+ */
 function processCardStyle(flipped) {
   const randomposition = () => (Math.random() - 0.5) * maxDistance * 2;
   const randomrotation = () => (Math.random() - 0.5) * 360;
 
-
   return {
     position: "absolute",
-    top: window.innerHeight / 2 - cardWidth / 2 + (Math.random() - 0.5) * 20,
-    left: window.innerWidth / 2 - cardWidth / 2 + (Math.random() - 0.5) * 20,
-    width: cardWidth,
-    height: cardWidth,
-    background: "blue",
+    left: window.innerWidth / 2 - 130 / 2 + (Math.random() - 0.5) * 20,
+    top: window.innerHeight / 2 - 120 / 2 + (Math.random() - 0.5) * 20,
+    width: 130,
+    height: 120,
+    backgroundImage: `url(${bluecard})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
     borderRadius: 5,
-    border: "1px solid black",
-    transition: "all 1s ease-out",  // suggestion radians
-    transform: flipped ? `translate(${randomposition()}px,${randomposition()}px) rotate(${randomrotation()}deg)` : ''
+    // border: "1px solid black",
+    transition: "all 1s cubic-bezier(.15,.84,.84,1)",  // suggestion radians
+    transform: flipped ? `translate(${randomposition()}px,${randomposition()}px) rotate(${randomrotation()}deg)` : '',
   };
 }
