66cc0a93bca5463ad64b7ffdf025d21a6aea29df
lucaspontoexe <ltx16@hotmail.com>
2020-11-13 18:40:46 -0300

    add utility function

---
+++ src/types/Card.d.ts
@@ -0,0 +1,20 @@
+export interface Card {
+    tipo_arquivo: string;
+    nome: string;
+    titulo: string;
+    legenda: string;
+    local: string;
+    data: string;
+    tipo: string;
+    fotografo: string;
+    notas: string;
+    creditos: string;
+    fonte: string;
+    tipo_licenca: string;
+    formato: string;
+    horizontal_res: string;
+    vertical_res: string;
+    dimensoes_foto: string;
+    dimensoes_objeto: string;
+    link: string;
+}
